78ad6a36ae87aa949b0aa62ddfd938ef48f994b1
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2018-03-28 19:29:47 +0100

    Floating search view. Material Drawer. About Libraries.Basic Espresso tests.

---
+++ app/src/androidTest/java/uk/co/oliverdelange/wcr_android_kt/SearchDrawerTest.kt
@@ -0,0 +1,42 @@
+package uk.co.oliverdelange.wcr_android_kt
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SearchDrawerTest {
+
+    lateinit var searchString: String
+
+    @Rule @JvmField
+    val mActivityRule = ActivityTestRule(MapsActivity::class.java)
+
+    @Before
+    fun initValidString() {
+        // Specify a valid string.
+        searchString = "searchme"
+    }
+
+    @Test
+    fun checkDrawerMenuItems() {
+        onView(withId(R.id.search_bar_left_action_container))
+                .perform(click())
+
+        onView(withId(R.id.material_drawer_name))
+                .check(matches(withText("About")))
+    }
+
+    @Test
+    fun checkSearchBarHintText() {
+        onView(withId(R.id.search_bar_text))
+                .check(matches(withHint("Search for crags and climbs")))
+    }
+}
--- app/src/main/java/uk/co/oliverdelange/wcr_android_kt/MapsActivity.kt
+++ app/src/main/java/uk/co/oliverdelange/wcr_android_kt/MapsActivity.kt
@@ -1,18 +1,34 @@
 package uk.co.oliverdelange.wcr_android_kt
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
-
-import com.google.android.gms.maps.CameraUpdateFactory
+import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import co.zsmb.materialdrawerkt.builders.accountHeader
+import co.zsmb.materialdrawerkt.builders.drawer
+import co.zsmb.materialdrawerkt.draweritems.badgeable.primaryItem
+import co.zsmb.materialdrawerkt.imageloader.drawerImageLoader
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.mikepenz.aboutlibraries.Libs
+import com.mikepenz.aboutlibraries.LibsBuilder
+import com.mikepenz.google_material_typeface_library.GoogleMaterial
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem
+import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader
+import com.mikepenz.materialdrawer.util.DrawerImageLoader
+import com.mikepenz.materialdrawer.util.DrawerUIUtils
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_maps.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var slidingDrawer: Drawer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,23 +37,55 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        initialiseDrawer()
+        initialiseFloatingSearchBar()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private fun initialiseDrawer() {
+        slidingDrawer = drawer {
+            accountHeader {
+                background = R.drawable.nature
+            }
+            primaryItem(R.string.menu_about) {
+                iicon = GoogleMaterial.Icon.gmd_help
+                selectable = false
+                onClick { _ ->
+                    LibsBuilder()
+                            .withActivityStyle(Libs.ActivityStyle.LIGHT_DARK_TOOLBAR)
+                            .withAboutAppName("We Climb Rocks")
+                            .withLicenseShown(true)
+                            .withActivityTitle("About")
+                            .withAboutIconShown(true)
+                            .withAboutVersionShown(true)
+                            .withAboutDescription("We Climb Rocks is a platform for sharing climbing topos, and easily locating routes." +
+                                    "<br /><b>For support, email <a href='mailto:weclimbrocks@oliverdelange.co.uk'>weclimbrocks@oliverdelange.co.uk</a></b>" +
+                                    "<br /><br />" +
+                                    "Below is a list of Open Source libraries used in this app.")
+                            .start(applicationContext)
+                    false
+                }
+            }
+        }
+
+        drawerImageLoader {
+            placeholder { ctx, tag ->
+                DrawerUIUtils.getPlaceHolder(ctx)
+            }
+            set { imageView, uri, placeholder, tag ->
+                Picasso.with(imageView.context).load(uri).placeholder(placeholder).into(imageView)
+            }
+            cancel { imageView ->
+                Picasso.with(imageView.context).cancelRequest(imageView)
+            }
+        }
+    }
+
+    private fun initialiseFloatingSearchBar() {
+        floating_search_view.attachNavigationDrawerToMenuButton(slidingDrawer.drawerLayout)
     }
 }
