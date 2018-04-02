9f966fe09c30134064fbd8cf607d36a864315fca
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2018-04-02 10:53:51 +0100

    Timber.

--- app/src/debug/java/uk/co/oliverdelange/wcr_android_kt/WcrAppDebug.kt
+++ app/src/debug/java/uk/co/oliverdelange/wcr_android_kt/WcrAppDebug.kt
@@ -2,12 +2,14 @@ package uk.co.oliverdelange.wcr_android_kt
 
 import com.facebook.stetho.Stetho
 import dagger.android.HasActivityInjector
+import timber.log.Timber
 import uk.co.oliverdelange.wcr_android_kt.di.AppInjector
 
 class WcrAppDebug : WcrApp(), HasActivityInjector {
     override fun onCreate() {
         super.onCreate()
         AppInjector.init(this)
+        Timber.plant(Timber.DebugTree())
         Stetho.initializeWithDefaults(this)
     }
 }
--- app/src/main/java/uk/co/oliverdelange/wcr_android_kt/repository/LocationRepository.kt
+++ app/src/main/java/uk/co/oliverdelange/wcr_android_kt/repository/LocationRepository.kt
@@ -1,5 +1,6 @@
 package uk.co.oliverdelange.wcr_android_kt.repository
 
+import timber.log.Timber
 import uk.co.oliverdelange.wcr_android_kt.db.LocationDao
 import uk.co.oliverdelange.wcr_android_kt.model.Location
 import uk.co.oliverdelange.wcr_android_kt.util.AppExecutors
@@ -9,6 +10,7 @@ class LocationRepository @Inject constructor(val locationDao: LocationDao, val a
 
     fun save(location: Location) {
         appExecutors.diskIO().execute {
+            Timber.d("Saving location: %s", location)
             locationDao.save(location)
         }
     }
