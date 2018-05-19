b4a4e443d6c6e1bf26e8179a1e0bcba5a9533471
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2018-05-20 00:41:35 +0100

    Quick fix for coloured topo routes.

--- app/src/main/java/uk/co/oliverdelange/wcr_android_kt/ui/view/TopoImageView.kt
+++ app/src/main/java/uk/co/oliverdelange/wcr_android_kt/ui/view/TopoImageView.kt
@@ -3,49 +3,48 @@ package uk.co.oliverdelange.wcr_android_kt.ui.view
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import uk.co.oliverdelange.wcr_android_kt.model.GradeColour
 import uk.co.oliverdelange.wcr_android_kt.model.Route
 
 //TODO Open source this extension?
 class TopoImageView(c: Context, a: AttributeSet) : TouchImageView(c, a) {
 
     val routes: MutableList<Route> = mutableListOf()
-    val path = Path()
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.save()
         canvas.concat(matrix)
         routes.forEach {
-            path.reset()
             //FIXME Logic on draw - prepopulate paths somewhere
-            canvas.drawPath(getRoutePath(path, it.path, drawable.intrinsicWidth, drawable.intrinsicHeight), paint)
+            val routePath = Path()
+            val routePoints = it.path
+            if (routePoints != null && routePoints.size > 1) {
+                val iterator = routePoints.iterator()
+                val firstPoint = iterator.next()
+                routePath.moveTo(firstPoint.first * drawable.intrinsicWidth, firstPoint.second * drawable.intrinsicHeight)
+                while (iterator.hasNext()) {
+                    val next = iterator.next()
+                    routePath.lineTo(next.first * drawable.intrinsicWidth, next.second * drawable.intrinsicHeight)
+                }
+            }
+            when (it.grade?.colour) {
+                GradeColour.GREEN -> paint.setARGB(100, 80, 220, 20)
+                GradeColour.ORANGE -> paint.setARGB(100, 220, 110, 0)
+                GradeColour.RED -> paint.setARGB(100, 255, 0, 0)
+                GradeColour.BLACK -> paint.setARGB(100, 0, 0, 0)
+            }
+            canvas.drawPath(routePath, paint)
         }
-        canvas.restore()
         printMatrixInfo()
     }
 
     private var paint = Paint().also {
         it.isAntiAlias = true
         it.isDither = true
-        it.color = -0x10000
         it.style = Paint.Style.STROKE
         it.strokeJoin = Paint.Join.ROUND
         it.strokeCap = Paint.Cap.ROUND
-        it.strokeWidth = 20f
+        it.strokeWidth = 10f
         it.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_OVER)
-        it.alpha = 0x80
     }
-}
-
-fun getRoutePath(rtn: Path, path: Set<Pair<Float, Float>>?, width: Int, height: Int): Path {
-    if (path != null && path.size > 1) {
-        val iterator = path.iterator()
-        val firstPoint = iterator.next()
-        rtn.moveTo((firstPoint.first * width), firstPoint.second * height)
-        while (iterator.hasNext()) {
-            val next = iterator.next()
-            rtn.lineTo(next.first * width, next.second * height)
-        }
-    }
-    return rtn
 }
