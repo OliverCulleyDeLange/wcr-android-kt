cc5a5cf3f3086013c2fa374a5d0fddb16a7a30a8
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2018-05-20 13:12:45 +0100

    Seperate path calculation from onDraw (badly)

--- app/src/main/java/uk/co/oliverdelange/wcr_android_kt/ui/view/TopoImageView.kt
+++ app/src/main/java/uk/co/oliverdelange/wcr_android_kt/ui/view/TopoImageView.kt
@@ -1,13 +1,37 @@
 package uk.co.oliverdelange.wcr_android_kt.ui.view
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Path
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.util.AttributeSet
 import uk.co.oliverdelange.wcr_android_kt.model.GradeColour
 import uk.co.oliverdelange.wcr_android_kt.model.Route
 
 class TopoImageView(c: Context, a: AttributeSet) : TouchImageView(c, a) {
+
+    //TODO Better way of refreshing paths once image loaded?
+    override fun setImageResource(resId: Int) {
+        super.setImageResource(resId)
+        refreshPaths()
+    }
+
+    override fun setImageBitmap(bm: Bitmap) {
+        super.setImageBitmap(bm)
+        refreshPaths()
+    }
+
+    override fun setImageDrawable(drawable: Drawable?) {
+        super.setImageDrawable(drawable)
+        refreshPaths()
+    }
+
+    override fun setImageURI(uri: Uri?) {
+        super.setImageURI(uri)
+        refreshPaths()
+    }
 
     var routes: List<Route> = emptyList()
         set(value) {
@@ -15,20 +39,21 @@ class TopoImageView(c: Context, a: AttributeSet) : TouchImageView(c, a) {
             selectedRoute = routes[0]
         }
 
+    val paths = mutableMapOf<Long, Path>()
+
     var selectedRoute: Route? = null
         set(value) {
             field = value
             invalidate()
         }
 
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        canvas.concat(matrix)
+    private fun refreshPaths() {
+        paths.clear()
         routes.forEach {
-            //FIXME Logic on draw - prepopulate paths somewhere
-            val routePath = Path()
             val routePoints = it.path
-            if (routePoints != null && routePoints.size > 1) {
+            val routeId = it.id
+            if (routePoints != null && routePoints.size > 1 && routeId != null) {
+                val routePath = Path()
                 val iterator = routePoints.iterator()
                 val firstPoint = iterator.next()
                 routePath.moveTo(firstPoint.first * drawable.intrinsicWidth, firstPoint.second * drawable.intrinsicHeight)
@@ -36,17 +61,25 @@ class TopoImageView(c: Context, a: AttributeSet) : TouchImageView(c, a) {
                     val next = iterator.next()
                     routePath.lineTo(next.first * drawable.intrinsicWidth, next.second * drawable.intrinsicHeight)
                 }
+                paths[routeId] = routePath
             }
-            when (it.grade?.colour) {
-                GradeColour.GREEN -> canvas.drawPath(routePath, greenRoutePaint)
-                GradeColour.ORANGE -> canvas.drawPath(routePath, orangeRoutePaint)
-                GradeColour.RED -> canvas.drawPath(routePath, redRoutePaint)
-                GradeColour.BLACK -> canvas.drawPath(routePath, blackRoutePaint)
-            }
-            if (selectedRoute?.id == it.id) canvas.drawPath(routePath, selectedRoutePaint)
         }
-        printMatrixInfo()
     }
 
-
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.concat(matrix)
+        routes.forEach {
+            if (paths.containsKey(it.id)) {
+                val routePath = paths[it.id]
+                when (it.grade?.colour) {
+                    GradeColour.GREEN -> canvas.drawPath(routePath, greenRoutePaint)
+                    GradeColour.ORANGE -> canvas.drawPath(routePath, orangeRoutePaint)
+                    GradeColour.RED -> canvas.drawPath(routePath, redRoutePaint)
+                    GradeColour.BLACK -> canvas.drawPath(routePath, blackRoutePaint)
+                }
+                if (selectedRoute?.id == it.id) canvas.drawPath(routePath, selectedRoutePaint)
+            }
+        }
+    }
 }
