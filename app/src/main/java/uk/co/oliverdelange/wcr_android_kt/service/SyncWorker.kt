2d913a39ae01ff6e00a2241d8f51e099d2dc24fb
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2019-05-06 10:19:59 +0100

    OMG i fixed it! Yay. Await Async task as its in a bg thread already!

--- app/src/main/java/uk/co/oliverdelange/wcr_android_kt/service/SyncWorker.kt
+++ app/src/main/java/uk/co/oliverdelange/wcr_android_kt/service/SyncWorker.kt
@@ -1,22 +1,21 @@
 package uk.co.oliverdelange.wcr_android_kt.service
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.work.RxWorker
 import androidx.work.WorkerParameters
+import com.google.android.gms.tasks.Tasks
 import com.google.firebase.firestore.FirebaseFirestore
 import io.reactivex.Observable
-import io.reactivex.Scheduler
 import io.reactivex.Single
-import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
 import uk.co.oliverdelange.wcr_android_kt.db.Location
 import uk.co.oliverdelange.wcr_android_kt.db.WcrDb
 
-class SyncLocationsWorker(appContext: Context, workerParams: WorkerParameters) : RxWorker(appContext, workerParams) {
-    override fun getBackgroundScheduler(): Scheduler {
-        return Schedulers.io()
-    }
 
+class SyncLocationsWorker(appContext: Context, workerParams: WorkerParameters) : RxWorker(appContext, workerParams) {
+
+    @SuppressLint("WrongThread") //TODO is this right?
     override fun createWork(): Single<Result> {
         Timber.d("${Thread.currentThread().name} Syncing local locations to firestore")
         val localDb = WcrDb.getInstance(applicationContext)
@@ -31,7 +30,6 @@ class SyncLocationsWorker(appContext: Context, workerParams: WorkerParameters) :
                             .flatMapCompletable {
                                 Timber.d("${Thread.currentThread().name} Marking Location $it as uploaded")
                                 localDb.locationDao().markAsUploaded(it)
-                                        .subscribeOn(Schedulers.io())
                             }
                 }
                 .toSingleDefault(Result.success())
@@ -39,22 +37,59 @@ class SyncLocationsWorker(appContext: Context, workerParams: WorkerParameters) :
 
     private fun saveLocationToRemoteDb(firebaseFirestore: FirebaseFirestore, location: Location): Single<String> {
         return Single.create<String> { emitter ->
-            //            Timber.d("Saving location to firestore : ${location.id}")
-            firebaseFirestore.collection("locations")
+            Timber.d("${Thread.currentThread().name} Saving location to firestore : ${location.id}")
+            val setTask = firebaseFirestore.collection("locations")
                     .document(location.id)
                     .set(location)
-                    .addOnSuccessListener {
-                        Timber.d("${Thread.currentThread().name} Location saved to firestore: ${location.id} ")
-                        emitter.onSuccess(location.id)
-                    }
-                    .addOnFailureListener { err ->
-                        Timber.e(err, "Failed to add location to firestore: ${location.id}")
-                        emitter.onError(err)
-                        //         TODO Show error if not signed in
-                        //        if (err is FirebaseFirestoreException && err.code == FirebaseFirestoreException.Code.PERMISSION_DENIED) {
-                        //            Timber.d("User tried to submit when not logged in")
-                        //        }
-                    }
+            try {
+                //TODO Write blog post on how to do this!
+                Tasks.await(setTask)
+                Timber.d("${Thread.currentThread().name} Location saved to firestore: ${location.id} ")
+                emitter.onSuccess(location.id)
+            } catch (e: Exception) {
+                Timber.e(e, "Failed to add location to firestore: ${location.id}")
+                emitter.onError(e)
+                //         TODO Show error if not signed in
+                //        if (err is FirebaseFirestoreException && err.code == FirebaseFirestoreException.Code.PERMISSION_DENIED) {
+                //            Timber.d("User tried to submit when not logged in")
+                //        }
+            }
         }
     }
 }
+
+// Kotlin test script
+//import io.reactivex.*
+//import io.reactivex.android.schedulers.AndroidSchedulers
+//import io.reactivex.schedulers.Schedulers
+//
+//fun saveRemote(id: String) : Single<String> {
+//    println ("saveRemote $id ${Thread.currentThread().name}... ")
+//    return Single.just(id)
+//}
+//
+//fun markAsUploaded(id: String) : Completable {
+//    println( "markAsUploaded $id ${Thread.currentThread().name}... ")
+//    return Completable.complete()
+//}
+//
+//println( "1 ${Thread.currentThread().name}")
+//
+//val source = Maybe.just(listOf("1","2","3"))
+//val doStuff = source.flatMapCompletable { locations ->
+//    println("2 ${Thread.currentThread().name}... ")
+//    val observableArray = locations.map { saveRemote(it).toObservable() }.toTypedArray()
+//    val uploadedLocationIdStream = Observable.mergeArrayDelayError(*observableArray)
+//    uploadedLocationIdStream.flatMapCompletable {
+//        println("3 ${Thread.currentThread().name}... ")
+//        markAsUploaded(it)
+//    }
+//}
+//
+//doStuff
+//.subscribeOn(Schedulers.io())
+//.subscribe {
+//    println ( "4... .. ")
+//}
+//
+//Thread.sleep(3000)
