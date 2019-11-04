25b7b79c8cc33932c01b93fdfd95732c795715dc
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2019-11-04 15:28:23 +0000

    Don't send verbose logs to Crashlytics

--- app/src/main/java/uk/co/oliverdelange/wcr_android_kt/util/CrashlyticsTimberTree.kt
+++ app/src/main/java/uk/co/oliverdelange/wcr_android_kt/util/CrashlyticsTimberTree.kt
@@ -1,10 +1,12 @@
 package uk.co.oliverdelange.wcr_android_kt.util
 
+import android.util.Log
 import com.crashlytics.android.Crashlytics
 import timber.log.Timber
 
 class CrashlyticsTree : Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        if (priority == Log.VERBOSE) return
         Crashlytics.log("$priority  $message")
     }
 }
