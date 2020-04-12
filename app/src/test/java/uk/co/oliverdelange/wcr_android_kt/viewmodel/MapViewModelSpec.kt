974a3b2f74909a5ed4c374669f3808887af724dd
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2020-04-12 16:32:39 +0100

    Upgrade to gradle 6.3

--- app/src/test/java/uk/co/oliverdelange/wcr_android_kt/viewmodel/MapViewModelSpec.kt
+++ app/src/test/java/uk/co/oliverdelange/wcr_android_kt/viewmodel/MapViewModelSpec.kt
@@ -1,18 +1,18 @@
 package uk.co.oliverdelange.wcr_android_kt.viewmodel
 
-//import io.kotest.core.spec.style.FreeSpec
-//
-//class MapViewModelSpec : FreeSpec({
-//
-//    beforeTest {
-//        println("BEFORE")
-//    }
-//
-//    afterTest {
-//        println("AFTER")
-//    }
-//
-//    "buildVersionClicked" {
-//        println("RUNNING")
-//    }
-//})
+import io.kotest.core.spec.style.FreeSpec
+
+class MapViewModelSpec : FreeSpec({
+
+    beforeTest {
+        println("BEFORE")
+    }
+
+    afterTest {
+        println("AFTER")
+    }
+
+    "buildVersionClicked" {
+        println("RUNNING")
+    }
+})
