9a1e1405dbf3b392a23d5b4d3c8205070e3f2e47
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2020-04-12 14:51:41 +0100

    Tried out Junit5 with Mockk - Annotation based mocks work fine here

---
+++ app/src/test/java/uk/co/oliverdelange/wcr_android_kt/viewmodel/MapViewModelSpec.kt
@@ -0,0 +1,18 @@
+package uk.co.oliverdelange.wcr_android_kt.viewmodel
+
+//import io.kotest.core.spec.style.FreeSpec
+//
+//class MapViewModelSpec : FreeSpec({
+//
+//    beforeTest {
+//        println("BEFORE")
+//    }
+//
+//    afterTest {
+//        println("AFTER")
+//    }
+//
+//    "buildVersionClicked" {
+//        println("RUNNING")
+//    }
+//})
